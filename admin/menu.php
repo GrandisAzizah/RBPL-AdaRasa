9720c0d8a4fe9c53ae98dd7fc53c8b6eb821e693
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-03-15 15:55:48 +0700

    minor edit

--- admin/menu.php
+++ admin/menu.php
@@ -19,15 +19,15 @@ $menu = query("SELECT * FROM menu ORDER BY nama_menu ASC");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
-    <link rel="stylesheet" href="menu.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="menu.css">
 </head>
 
 <body>
     <div class="container-main">
-        <div class="header-nav mt-3">
+        <div class="header-nav mt-3 mb-5">
             <a href="berandaAdmin.php" class="header-nav-left">
                 <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M31.6667 19H6.33337M6.33337 19L15.8334 9.5M6.33337 19L15.8334 28.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -42,35 +42,39 @@ $menu = query("SELECT * FROM menu ORDER BY nama_menu ASC");
         </div>
 
         <?php if (count($menu) == 0) : ?>
-            <p class="text-center mt-5" style="color: #979696; margin-top: 20px; height: 70vh; display: flex; align-items: center; justify-content: center;"">Belum ada menu yang ditambahkan</p>
+            <p class="text-center mt-5" style="color: #979696; margin-top: 20px; height: 70vh; display: flex; align-items: center; justify-content: center;">Belum ada menu yang ditambahkan</p>
         <?php else : ?>
             <?php foreach ($menu as $row) : ?>
-            <a href=" showBahan.php?id_menu=<?= $row['id_menu'] ?>" style="text-decoration:none; color:inherit; display:contents;">
-            <div class=" container-menu mt-4">
-                <div class="card">
-                    <div class="row g-0">
-                        <!-- Gambar -->
-                        <div class="col-auto">
-                            <img src="<?= $row['gambar_menu'] ?>" class="menu-img" alt="...">
-                        </div>
-                        <!-- Isi -->
-                        <div class="col">
-                            <div class="card-body">
-                                <h5 class="card-title"><?= $row['nama_menu'] ?></h5>
-                                <p class="card-text"><?= $row['harga_menu'] ?></p>
+                <div class=" container-menu mt-2">
+                    <div class="card">
+                        <div class="row g-0">
+                            <!-- Gambar -->
+                            <a href=" showBahan.php?id_menu=<?= $row['id_menu'] ?>" style="text-decoration:none; color:inherit; display:contents;">
+                                <div class="col-auto">
+                                    <img src="<?= $row['gambar_menu'] ?>" class="menu-img" alt="...">
+                                </div>
+                                <!-- Isi -->
+                                <div class="col">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?= $row['nama_menu'] ?></h5>
+                                        <p class="card-text"><?= $row['harga_menu'] ?></p>
+                                    </div>
+                                </div>
+                            </a>
+                            <!-- Tombol Edit dan Hapus -->
+                            <div class="col-auto menu-btn d-flex align-items-center gap-2 p-2 align-self-end">
+                                <a href="editMenu.php?id_menu=<?= $row['id_menu'] ?>" class="edit-btn btn btn-dark btn-sm">Edit</a>
+                                <a href="#" class="delete-btn btn btn-danger btn-sm" onclick="setHapusUrl('hapusMenu.php?id_menu=<?= $row['id_menu'] ?>')">Hapus</a>
                             </div>
-                        </div>
-                        </a>
-                        <!-- Tombol Edit dan Hapus -->
-                        <div class="col-auto menu-btn d-flex align-items-center gap-2 p-2 align-self-end">
-                            <a href="editMenu.php?id_menu=<?= $row['id_menu'] ?>" class="edit-btn btn btn-dark btn-sm">Edit</a>
-                            <a href="#" class="delete-btn btn btn-danger btn-sm" onclick="setHapusUrl('hapusMenu.php?id_menu=<?= $row['id_menu'] ?>')">Hapus</a>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+    </div>
+
 
     <!-- Modal Konfirmasi Hapus -->
     <div class="modal fade" id="modalHapus" tabindex="-1">
