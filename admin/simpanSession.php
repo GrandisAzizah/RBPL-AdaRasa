d6359fe3397e555625401bab8c8ea0d399064b55
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-04-24 07:22:21 +0700

    added: untuk menyimpan session dalam input pesanan

---
+++ admin/simpanSession.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["login"])) {
+    header("location: login.php");
+    exit;
+}
+
+$_SESSION['pesanan'] = [
+    'fk_customer' => $_POST['nama_pelanggan'],
+    'fk_menu' => $_POST['nama_pesanan'],
+    'fk_pesanan_varian' => $_POST['takaran'],
+    'jumlah' => (int)$_POST['jumlah'],
+    'metode' => $_POST['metode_pengantaran'],
+    'tanggal_antar' => $_POST['tanggal_antar'],
+    'catatan' => $_POST['catatan_khusus_pemesanan'],
+];
+
+header('location: inputBahanPesan.php');
+exit;
