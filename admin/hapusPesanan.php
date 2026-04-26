2dca3c2be9dafbfb2d7b0b267ba1b22c7e7e6be6
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-04-26 23:38:42 +0700

    fix

--- admin/hapusPesanan.php
+++ admin/hapusPesanan.php
@@ -1,21 +1,15 @@
 <?php
-
 session_start();
-// user belum login
 if (!isset($_SESSION["login"])) {
     header("location: ../login.php");
     exit;
 }
-
 require '../functions.php';
-// ambil id yang dikirim ketika tombol dipencet
-// id dikirim lewat url
-$id_pesanan = $_GET["id_pesanan"];
+
+$id_pesanan = (int)$_GET['id_pesanan'];
 
 if (hapusPesanan($id_pesanan) > 0) {
-    header("location: showBahan.php?id_menu=" . $id_menu);
+    header("location: pesanan.php");
 } else {
-    echo "Data gagal dihapus";
-    // menampilkan kenapa gagal
-    echo mysqli_error($conn);
+    echo "Data gagal dihapus: " . mysqli_error($conn);
 }
