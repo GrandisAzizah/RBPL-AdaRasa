e02fe4add12b4b9e59bd59aa7efb7be04130f921
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-04-27 21:55:16 +0700

    edit fk_pesanan_varian

--- admin/simpanSession.php
+++ admin/simpanSession.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION["login"])) {
 $_SESSION['pesanan'] = [
     'fk_customer' => $_POST['nama_pelanggan'],
     'fk_menu' => $_POST['nama_pesanan'],
-    'fk_pesanan_varian' => $_POST['takaran'],
+    'fk_pesanan_varian' => !empty($_POST['takaran']) ? (int)$_POST['takaran'] : null,
     'jumlah' => (int)$_POST['jumlah'],
     'metode' => $_POST['metode_pengantaran'],
     'tanggal_antar' => $_POST['tanggal_antar'],
