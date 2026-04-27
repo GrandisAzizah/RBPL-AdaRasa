c38209481a212bdf96fa7d5b52cb5cb967c08d86
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-04-28 01:51:58 +0700

    update query

--- admin/updateStok.php
+++ admin/updateStok.php
@@ -18,25 +18,50 @@ if (empty($nama_bahan) || $jumlah_beli <= 0) {
     exit;
 }
 
-if ($checked == 1) {
-    $query = "UPDATE stok_bahan SET stok_tersedia = stok_tersedia + $jumlah_beli 
-              WHERE nama_bahan_stok = '$nama_bahan'";
-    mysqli_query($conn, $query);
-    $updateStatus = "UPDATE detail_pesanan_bahan dpb 
-                     JOIN bahan_baku bb ON dpb.fk_bahan_detail = bb.id_bahan
-                     SET dpb.status_beli = 1 
-                     WHERE bb.nama_bahan = '$nama_bahan' AND dpb.status_beli = 0";
-    mysqli_query($conn, $updateStatus);
-} else {
-    $query = "UPDATE stok_bahan SET stok_tersedia = stok_tersedia - $jumlah_beli 
-              WHERE nama_bahan_stok = '$nama_bahan' AND stok_tersedia >= $jumlah_beli";
-    mysqli_query($conn, $query);
+// if ($checked == 1) {
+//     $query = "UPDATE stok_bahan SET stok_tersedia = stok_tersedia + $jumlah_beli 
+//               WHERE nama_bahan_stok = '$nama_bahan'";
+//     mysqli_query($conn, $query);
+//     $updateStatus = "UPDATE detail_pesanan_bahan dpb 
+//                      JOIN bahan_baku bb ON dpb.fk_bahan_detail = bb.id_bahan
+//                      SET dpb.status_beli = 1 
+//                      WHERE bb.nama_bahan = '$nama_bahan' AND dpb.status_beli = 0";
+//     mysqli_query($conn, $updateStatus);
+// } else {
+//     $query = "UPDATE stok_bahan SET stok_tersedia = stok_tersedia - $jumlah_beli 
+//               WHERE nama_bahan_stok = '$nama_bahan' AND stok_tersedia >= $jumlah_beli";
+//     mysqli_query($conn, $query);
 
-    $updateStatus = "UPDATE detail_pesanan_bahan dpb 
-                     JOIN bahan_baku bb ON dpb.fk_bahan_detail = bb.id_bahan
-                     SET dpb.status_beli = 0 
-                     WHERE bb.nama_bahan = '$nama_bahan' AND dpb.status_beli = 1";
-    mysqli_query($conn, $updateStatus);
+//     $updateStatus = "UPDATE detail_pesanan_bahan dpb 
+//                      JOIN bahan_baku bb ON dpb.fk_bahan_detail = bb.id_bahan
+//                      SET dpb.status_beli = 0 
+//                      WHERE bb.nama_bahan = '$nama_bahan' AND dpb.status_beli = 1";
+//     mysqli_query($conn, $updateStatus);
+// }
+
+$stmt = $conn->prepare(
+    $checked == 1
+        ? "UPDATE stok_bahan SET stok_tersedia = stok_tersedia + ? WHERE nama_bahan_stok = ?"
+        : "UPDATE stok_bahan SET stok_tersedia = stok_tersedia - ? WHERE nama_bahan_stok = ? AND stok_tersedia >= ?"
+);
+if ($checked == 1) {
+    $stmt->bind_param('ds', $jumlah_beli, $nama_bahan);
+} else {
+    $stmt->bind_param('dsi', $jumlah_beli, $nama_bahan, $jumlah_beli);
 }
+$stmt->execute();
+$stmt->close();
+
+$statusQry = $conn->prepare(
+    "UPDATE detail_pesanan_bahan dpb 
+     JOIN bahan_baku bb ON dpb.fk_bahan_detail = bb.id_bahan
+     SET dpb.status_beli = ? 
+     WHERE bb.nama_bahan = ? AND dpb.status_beli <> ?"
+);
+$newStatus = $checked == 1 ? 1 : 0;
+$oldStatus = $checked == 1 ? 0 : 1;
+$statusQry->bind_param('isi', $newStatus, $nama_bahan, $oldStatus);
+$statusQry->execute();
+$statusQry->close();
 
 echo json_encode(['success' => true]);
