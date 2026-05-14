33a710bfb86b90ffb9962ffe13ccf54e076e3fd3
GrandisAzizah <124240045@student.upnyk.ac.id>
2026-05-14 21:55:52 +0700

    edit query

--- admin/hapusPelanggan.php
+++ admin/hapusPelanggan.php
@@ -1,21 +1,17 @@
 <?php
-
 session_start();
-// user belum login
 if (!isset($_SESSION["login"])) {
     header("location: ../login.php");
     exit;
 }
 
 require '../functions.php';
-// ambil id yang dikirim ketika tombol dipencet
-// id dikirim lewat url
-$id_pelanggan = $_GET["id_pelanggan"];
+require '../alamat.php';
 
-if (hapusPelanggan($id_pelanggan) > 0) {
+$id_pelanggan = isset($_GET["id_pelanggan"]) ? (int)$_GET["id_pelanggan"] : 0;
+
+if ($id_pelanggan > 0 && hapusPelanggan($id_pelanggan) > 0) {
     header("location: pelanggan.php");
 } else {
     echo "Data gagal dihapus";
-    // menampilkan kenapa gagal
-    echo mysqli_error($conn);
 }
